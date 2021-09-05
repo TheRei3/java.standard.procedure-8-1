9dfd3ea81f6651159ff416163e5dd4e45f378dae
admin <admin@host.docker.internal>
2021-09-06 08:11:49 +0900

    ex8no1

---
+++ ex8no1/src/ex8no1/ex8no1.java
@@ -0,0 +1,17 @@
+package ex8no1;
+
+public class ex8no1 {
+	public static void main(String[] args) {
+		try {
+			try { } catch (Exception e) { }
+		} catch (Exception e) {
+//			try { } catch (Exception e) { }
+		}
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
